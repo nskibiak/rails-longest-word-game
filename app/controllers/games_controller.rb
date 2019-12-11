8796401f2a166b42ea98d72f452b0d2fab1db49d
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-11 17:13:23 +0900

    changed the way the params token is converted from string into array to allow for better formatting on final view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,10 +9,11 @@ class GamesController < ApplicationController
     require 'open-uri'
 
     @guess = params[:guess].downcase
-    @option_letters = params[:token].downcase
+    @option_letters = params[:token].downcase.gsub(/[^a-z]/, '').split('')
+    # @clean_array = @option_letters
 
     if !@guess.chars.all? { |letter| @guess.chars.count(letter) == @option_letters.count(letter) }
-      @result = "Sorry, but #{@guess.upcase!} can't be built out of #{@option_letters}"
+      @result = "Sorry, but #{@guess.upcase!} can't be built out of #{@option_letters.join(', ').upcase}"
     elsif JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@guess}").read)["found"] != true
       @result = "Sorry, but #{@guess.upcase!} does not seem to be a valid English word."
     else
