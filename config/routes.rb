7dd0ef784801acdd568084711215d01e089e435e
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-11 12:12:48 +0900

    finished styling on input fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # VERB PATH, to: 'CONTROLLER#ACTION'
   get 'new', to: 'games#new', as: :new
-  get 'score', to: 'games#score', as: :score
+  # get 'score', to: 'games#score'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'score', to: 'games#score', as: :score
 end
