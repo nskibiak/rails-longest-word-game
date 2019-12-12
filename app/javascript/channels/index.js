76f0bd13ca51bbc00383b6b8d232903f2f857983
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-12 21:30:24 +0900

    added some (commented out) JavaScript to allow the user to pick letters by clicking on them

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,12 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+
+// const inputField = document.querySelector("#input");
+// const letters = document.querySelectorAll('.letter');
+// letters.forEach(letter => {
+//   letter.addEventListener('click', () => {
+//     inputField.value += letter.innerHTML;
+//   });
+// });
